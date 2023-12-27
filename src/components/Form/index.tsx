51c395d21ad5907f91f634dfd61a948643376af6
Yure Rafael <yurerafael01@gmail.com>
2023-12-27 16:24:47 -0300

    fiz: image empty

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -112,7 +112,6 @@ const FormFormik:React.FC = () => {
 
             if(regex.test(values.name)) errors.name = 'Nome não pode incluir caracteres especiais.'
             else if(value <= 0) errors.value = 'O produto deve custar mais que 0.'
-            //else if(!values.file) errors.file = 'Necessário imagem.'
 
             return errors
           }}
